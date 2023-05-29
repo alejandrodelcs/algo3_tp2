be856d4df5f707e9cd82630cd90be92412cc5cd1
Bruno Contreras <bcontreras@fi.uba.ar>
2023-05-29 15:40:54 -0300

    feat: added State interface and the implementing class
    Modification of the Tower class to remove conditionals and use the State class.

--- src/main/java/edu/fiuba/algo3/modelo/Defense/WhiteTower.java
+++ src/main/java/edu/fiuba/algo3/modelo/Defense/WhiteTower.java
@@ -6,21 +6,7 @@ public class WhiteTower extends Tower{
             this.credits = 10;
             this.rangeAttack = 3;
             this.damage = 1;
-            this.operational_state=false;
         }
-    public void Attack(Enemy enemy){
-        if (!this.operational_state){
-            throw new TowerIsUnderConstruction();
-        }else{
-            if(!enemy.isWithinAttackRange()){
-                throw new EnemyIsOutOfRange();
-            }else{
-                enemy.receiveDamage(damage);
-            }
-        }
-    }
-    public void constructionFinished (){
-        this.operational_state=true;
-    }
+
 }
 
