6121f4ee7141eaa0fcd430a44f072860d40fcd24
Bruno Contreras <bcontreras@fi.uba.ar>
2023-06-02 01:02:42 -0300

    feat: create Occupiable class

--- src/main/java/edu/fiuba/algo3/modelo/gameboard/NotOccupiable.java
+++
@@ -1,8 +0,0 @@
-package edu.fiuba.algo3.modelo.gameboard;
-
-import edu.fiuba.algo3.modelo.exceptions.NonConstructibleArea;
-
-public class NotOccupiable implements Occupiable{
-    @Override
-    public boolean itsOccupied(){throw new NonConstructibleArea();}
-}
