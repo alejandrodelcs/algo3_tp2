98e6d61e8e03837e1079f9071d277df7c2d5e5aa
alejandrodelcs <alejodc15@gmail.com>
2023-06-02 01:13:07 -0300

    add json map facade class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module edu.fiuba.algo3 {
     requires javafx.controls;
     requires java.datatransfer;
     requires java.desktop;
+    requires org.json;
     exports edu.fiuba.algo3;
 }
--- src/main/test/edu/fiuba/algo3/entrega_1/AlgoDefenseTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/AlgoDefenseTest.java
@@ -4,9 +4,14 @@ import edu.fiuba.algo3.modelo.AlgoDefense;
 import edu.fiuba.algo3.modelo.Defense.*;
 import edu.fiuba.algo3.modelo.Enemy.Spider;
 import edu.fiuba.algo3.modelo.GameBoard.NonConstructibleArea;
+import edu.fiuba.algo3.modelo.JsonFacade;
 import edu.fiuba.algo3.modelo.Player;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import org.json.JSONArray;
 
 import java.awt.*;
 
@@ -17,15 +22,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AlgoDefenseTest {
     @Test
-    public void test01(){
+    public void test01() {
         Player player = new Player("Player");
 
-        Assertions.assertEquals(100,player.getPlayerCredits());
+        Assertions.assertEquals(100, player.getPlayerCredits());
         Assertions.assertEquals(20, player.getPlayerLifePoints());
     }
 
     @Test
-    public void test02(){ //supuesto turno 0 inicial se construye white tower turno 1 esta construyendo turno 2 es operativo
+    public void test02() { //supuesto turno 0 inicial se construye white tower turno 1 esta construyendo turno 2 es operativo
 
         Player player = new Player("Player");
         WhiteTower whiteTower = new WhiteTower();
@@ -33,28 +38,28 @@ public class AlgoDefenseTest {
         AlgoDefense algoDefense = new AlgoDefense(player);
         ConstructionState construction = new ConstructionState();
         OperationalState operational = new OperationalState();
-        Point coordenatesWhiteTower = new Point(2,3);
-        Point coordenatesSilverTower = new Point(1,1);
+        Point coordenatesWhiteTower = new Point(2, 3);
+        Point coordenatesSilverTower = new Point(1, 1);
 
         algoDefense.buildsADefense(whiteTower, coordenatesWhiteTower);
         algoDefense.buildsADefense(silverTower, coordenatesSilverTower);
-        Assertions.assertEquals(construction.getClass(),whiteTower.getStatus().getClass());
-        Assertions.assertEquals(construction.getClass(),silverTower.getStatus().getClass());
+        Assertions.assertEquals(construction.getClass(), whiteTower.getStatus().getClass());
+        Assertions.assertEquals(construction.getClass(), silverTower.getStatus().getClass());
         algoDefense.newTurn();
-        Assertions.assertEquals(1,algoDefense.getTurn());
-        Assertions.assertEquals(operational.getClass(),whiteTower.getStatus().getClass());
-        Assertions.assertEquals(construction.getClass(),silverTower.getStatus().getClass());
+        Assertions.assertEquals(1, algoDefense.getTurn());
+        Assertions.assertEquals(operational.getClass(), whiteTower.getStatus().getClass());
+        Assertions.assertEquals(construction.getClass(), silverTower.getStatus().getClass());
         algoDefense.newTurn();
 
 
-        Assertions.assertEquals(2,algoDefense.getTurn());
-        Assertions.assertEquals(operational.getClass(),whiteTower.getStatus().getClass());
-        Assertions.assertEquals(operational.getClass(),silverTower.getStatus().getClass());
+        Assertions.assertEquals(2, algoDefense.getTurn());
+        Assertions.assertEquals(operational.getClass(), whiteTower.getStatus().getClass());
+        Assertions.assertEquals(operational.getClass(), silverTower.getStatus().getClass());
 
     }
 
     @Test
-    public void test03(){
+    public void test03() {
         Player player = new Player("Player");
         WhiteTower whiteTower = new WhiteTower();
 
@@ -65,41 +70,62 @@ public class AlgoDefenseTest {
         player.chargedCredits(95);
         Assertions.assertFalse(player.canBuy(value));
     }
+
     @Test
-    public void test04(){
+    public void test04() {
         Player player = new Player("Player");
         WhiteTower whiteTower = new WhiteTower();
         SilverTower silverTower = new SilverTower();
         AlgoDefense algoDefense = new AlgoDefense(player);
-        Point coordenatesDirtPlot = new Point(2,3);
-        Point coordenatesStonePlot = new Point(3,3);
+        Point coordenatesDirtPlot = new Point(2, 3);
+        Point coordenatesStonePlot = new Point(3, 3);
         ConstructionState construction = new ConstructionState();
 
-        algoDefense.buildsADefense(whiteTower,coordenatesDirtPlot);
+        algoDefense.buildsADefense(whiteTower, coordenatesDirtPlot);
 
-        Assertions.assertEquals(construction.getClass(),whiteTower.getStatus().getClass());
-        assertThrows(NonConstructibleArea.class,()->{
-            algoDefense.buildsADefense(silverTower, coordenatesStonePlot);;});
+        Assertions.assertEquals(construction.getClass(), whiteTower.getStatus().getClass());
+        assertThrows(NonConstructibleArea.class, () -> {
+            algoDefense.buildsADefense(silverTower, coordenatesStonePlot);
+            ;
+        });
     }
 
     @Test
-    public void test05(){
+    public void test05() {
         Player player = new Player("Player");
         WhiteTower whiteTower = new WhiteTower();
         SilverTower silverTower = new SilverTower();
         Spider spider = new Spider();
         AlgoDefense algoDefense = new AlgoDefense(player);
-        Point coordenatesDirtPlot = new Point(2,3);
-        Point coordenatesEnemy = new Point(3,3);
+        Point coordenatesDirtPlot = new Point(2, 3);
+        Point coordenatesEnemy = new Point(3, 3);
 
-        algoDefense.buildsADefense(whiteTower,coordenatesDirtPlot);
+        algoDefense.buildsADefense(whiteTower, coordenatesDirtPlot);
         algoDefense.newTurn();
         algoDefense.newTurn();
-        algoDefense.spawnAnEnemy(spider,coordenatesEnemy);
-        boolean isOnRange = algoDefense.enemyIsOnRange(whiteTower,coordenatesEnemy,coordenatesDirtPlot);
-        whiteTower.Attack(spider,isOnRange);
+        algoDefense.spawnAnEnemy(spider, coordenatesEnemy);
+        boolean isOnRange = algoDefense.enemyIsOnRange(whiteTower, coordenatesEnemy, coordenatesDirtPlot);
+        whiteTower.Attack(spider, isOnRange);
 
-        assertEquals(1,spider.getHealth());
+        assertEquals(1, spider.getHealth());
+
+    }
+
+
+    @Test
+    public void test06() {
+        JsonFacade jsonFacade = new JsonFacade();
+
+
+        try {
+            String path = "E:\\Algoritmos3\\algo3_tp2\\src\\main\\java\\edu\\fiuba\\algo3\\modelo\\mapa.json" ; //TODO: change relative path
+            JSONObject jsonObject = jsonFacade.readJSONFromFile(path);
+            JSONObject mapa = jsonFacade.getMapa(jsonObject);
+            jsonFacade.printMapa(mapa);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
