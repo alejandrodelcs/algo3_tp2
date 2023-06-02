f972857a00fff29b4ded169525b49c901aea6da5
Bruno Contreras <bcontreras@fi.uba.ar>
2023-06-02 01:04:28 -0300

    feat: create Occupiable class and the implementing class

--- src/main/java/edu/fiuba/algo3/modelo/GameBoard/Available.java
+++ src/main/java/edu/fiuba/algo3/modelo/GameBoard/Available.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.modelo.GameBoard;
 
-public class Avaliable implements Occupiable{
+public class Available implements Occupiable{
     @Override
     public boolean itsOccupied(){return false;}
 }
--- src/main/java/edu/fiuba/algo3/modelo/GameBoard/Ocuppied.java
+++ src/main/java/edu/fiuba/algo3/modelo/GameBoard/Ocuppied.java
@@ -1,2 +1,6 @@
-package edu.fiuba.algo3.modelo.GameBoard;public class Ocuppied {
+package edu.fiuba.algo3.modelo.GameBoard;
+
+public class Ocuppied implements Occupiable{
+    @Override
+    public boolean itsOccupied(){return true;}
 }
