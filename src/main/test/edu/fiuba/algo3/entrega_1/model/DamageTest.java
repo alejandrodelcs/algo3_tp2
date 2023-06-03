72238ce48278661156af878b71268080ed9eb3cd
liu <odillette@gmail.com>
2023-06-03 16:11:06 -0300

    feat: Damage creation and instance

--- src/main/test/edu/fiuba/algo3/entrega_1/model/DamageTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/model/DamageTest.java
@@ -3,16 +3,20 @@ package edu.fiuba.algo3.entrega_1.model;
 import edu.fiuba.algo3.modelo.damage.Damage;
 import edu.fiuba.algo3.modelo.health.Damageable;
 import edu.fiuba.algo3.modelo.health.Health;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class DamageTest {
 
-    public void test01(){
+
+    @Test
+    public void damageAmountTenAppliedToADamageableEntityWithTenLifePointsEntityEndUpsDead(){
         Damage damage = new Damage(10);
         Health health = new Damageable(10);
 
-        damage.applyDamage(10);
-        health.equals(0);
+        damage.applyDamage(health);
 
+        Assertions.assertTrue(health.entityDied());
 
     }
 }
