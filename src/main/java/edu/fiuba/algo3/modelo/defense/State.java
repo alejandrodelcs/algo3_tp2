d1aa69b1d5afe836066aea8f47ada78a703318ae
alejandrodelcs <alejodc15@gmail.com>
2023-06-10 04:30:38 -0300

    refactor by ale

---
+++ src/main/java/edu/fiuba/algo3/modelo/defense/State.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.modelo.defense;
+
+import edu.fiuba.algo3.modelo.defense.Tower;
+import edu.fiuba.algo3.modelo.enemy.Enemy;
+
+import java.util.ArrayList;
+
+public interface State {
+    void attack(Tower tower, ArrayList<Enemy> enemies);
+
+}
