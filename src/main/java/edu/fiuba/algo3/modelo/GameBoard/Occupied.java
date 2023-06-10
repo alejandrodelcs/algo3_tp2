46b1fe564ca072d76c7014885073b57a9dad8fac
Alejandro Sanchez <alejodc15@gmail.com>
2023-06-10 00:25:27 -0300

    Delete src/main/java/edu/fiuba/algo3/modelo/GameBoard directory
    delete GameBoard directory

--- src/main/java/edu/fiuba/algo3/modelo/GameBoard/Occupied.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo.gameboard;
-
-public class Occupied implements PlotAvailability {
-
-    @Override
-    public boolean itsOccupied(){return true;}
-}
--- src/main/java/edu/fiuba/algo3/modelo/GameBoard/PlotAvailability.java
+++
@@ -1,5 +0,0 @@
-package edu.fiuba.algo3.modelo.gameboard;
-
-public interface PlotAvailability {
-     boolean itsOccupied();
-}
