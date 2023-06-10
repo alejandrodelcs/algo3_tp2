fb14677af908de5c2c48a20b7c32fe27ab0ea3a5
Bruno Contreras <bcontreras@fi.uba.ar>
2023-06-09 22:17:28 -0300

    test: Test corrections have been made

--- src/main/test/edu/fiuba/algo3/entrega_1/model/TowerTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/model/TowerTest.java
@@ -1,47 +1,49 @@
 package edu.fiuba.algo3.entrega_1.model;
 
-import edu.fiuba.algo3.modelo.Credit;
 import edu.fiuba.algo3.modelo.defense.*;
 import edu.fiuba.algo3.modelo.enemy.*;
-import edu.fiuba.algo3.modelo.exceptions.EnemyIsOutOfRange;
-import edu.fiuba.algo3.modelo.exceptions.TowerDoesNotExist;
-import edu.fiuba.algo3.modelo.exceptions.TowerIsUnderConstruction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.awt.*;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class TowerTest {
 
   @Test
-    void test01NewWhiteTowerCannotAttackTheEnemy() {
-
+    void test01NewWhiteTowerUnderConstructionCannotAttackEnemy() {
         TowerFactory factory = new WhiteTowerFactory();
         Point cordenates = new Point(3,3);
         Tower WhiteTower = factory.createTower(cordenates);
         Enemy enemy = mock(Ant.class);
+        when(enemy.getPoint()).thenReturn(new Point(4,4));
         ArrayList<Enemy> enemies = new ArrayList<>();
         enemies.add(enemy);
 
         WhiteTower.attack(enemies);
+        verify(enemy, never()).takeDamage(WhiteTower.getDamage());
         Assertions.assertFalse(enemy.enemyDied());
     }
     @Test
-    void test02NewSilverTowerCannotAttackTheEnemy() {
+    void test02NewSilverTowerUnderConstructionCannotAttackEnemy() {
         TowerFactory factory = new SilverTowerFactory();
         Point cordenates = new Point(3,3);
         Tower SilverTower = factory.createTower(cordenates);
         Enemy enemy = mock(Ant.class);
+        when(enemy.getPoint()).thenReturn(new Point(4,4));
         ArrayList<Enemy> enemies = new ArrayList<>();
         enemies.add(enemy);
 
         SilverTower.attack(enemies);
+        verify(enemy, never()).takeDamage(SilverTower.getDamage());
         Assertions.assertFalse(enemy.enemyDied());
+
+        SilverTower.attack(enemies);
+        verify(enemy, never()).takeDamage(SilverTower.getDamage());
+        Assertions.assertFalse(enemy.enemyDied());
+
     }
 
     @Test
