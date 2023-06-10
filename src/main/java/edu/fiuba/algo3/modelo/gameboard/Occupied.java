bd316f4c557f48fb40ce3eaff9cbb20679d373af
Bruno Contreras <bcontreras@fi.uba.ar>
2023-06-10 18:49:03 -0300

    fix: The states of the plots were recreated

---
+++ src/main/java/edu/fiuba/algo3/modelo/gameboard/Occupied.java
@@ -0,0 +1,9 @@
+
+package edu.fiuba.algo3.modelo.gameboard;
+
+public class Occupied implements PlotAvailability {
+
+    @Override
+    public boolean itsOccupied(){return true;}
+}
+
---
+++ src/main/java/edu/fiuba/algo3/modelo/gameboard/PlotAvailability.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.modelo.gameboard;
+
+public interface PlotAvailability {
+    boolean itsOccupied();
+}
