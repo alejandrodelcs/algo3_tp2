ec62b4f51d2b8622cff02fd651d8febeb94745fc
Ruyllex <ruymori630@gmail.com>
2023-06-12 13:31:10 -0300

    Changes by odill

--- src/main/java/edu/fiuba/algo3/modelo/enemy/MoleFactory.java
+++ src/main/java/edu/fiuba/algo3/modelo/enemy/MoleFactory.java
@@ -4,12 +4,13 @@ import edu.fiuba.algo3.modelo.Credit;
 import edu.fiuba.algo3.modelo.damage.Damage;
 import edu.fiuba.algo3.modelo.health.Health;
 import edu.fiuba.algo3.modelo.health.NotDamageable;
+import edu.fiuba.algo3.modelo.speed.Acceleration;
 import edu.fiuba.algo3.modelo.speed.Speed;
 
 public class MoleFactory implements EnemyFactory{
     @Override
     public Enemy createEnemy() {
-        Speed someSpeed = new Speed(1);
+        Speed someSpeed = new Acceleration(1);
         Damage someDamage = new Damage(2);
         Health someHealth = new NotDamageable(0);
         Credit someCredit = new Credit(0);
