d7a8cf21dd3dad2eea1624be1588ed8a5fbcd748
Bruno Contreras <bcontreras@fi.uba.ar>
2023-06-14 17:06:32 -0300

    test: correction Tower tests

--- src/main/test/edu/fiuba/algo3/entrega_1/model/TowerTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/model/TowerTest.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 import static org.mockito.Mockito.*;
 
-public class TowerTest {/*
+public class TowerTest {
 
   @Test
     void test01NewWhiteTowerUnderConstructionCannotAttackEnemy() {
@@ -24,7 +24,7 @@ public class TowerTest {/*
         enemies.add(enemyMock);
 
         WhiteTower.attack(enemies);
-        verify(enemyMock, never()).takeDamage(WhiteTower.getDamage());
+        verify(enemyMock, never()).takeDamage(new Damage(1));
         Assertions.assertFalse(enemyMock.enemyDied());
     }
     @Test
@@ -33,53 +33,95 @@ public class TowerTest {/*
         Point cordenates = new Point(3,3);
         Tower SilverTower = factory.createTower(cordenates);
         Enemy enemy = mock(Spider.class);
-        when(enemy.getPoint()).thenReturn(new Point(4,4));
+        when(enemy.getPoint()).thenReturn(new Point(9,9));
         ArrayList<Enemy> enemies = new ArrayList<>();
         enemies.add(enemy);
 
         SilverTower.attack(enemies);
-        verify(enemy, never()).takeDamage(SilverTower.getDamage());
+        verify(enemy, never()).takeDamage(new Damage(2));
         Assertions.assertFalse(enemy.enemyDied());
 
         SilverTower.attack(enemies);
-        verify(enemy, never()).takeDamage(SilverTower.getDamage());
+        verify(enemy, never()).takeDamage(new Damage(2));
         Assertions.assertFalse(enemy.enemyDied());
 
     }
 
     @Test
     void test03WhiteTowerAttackWhenEnemyIsWithinRangeEnemyTakesDamage() {
-        EnemyFactory factoryEnemies = new EnemyFactory();
-        Enemy ant = factoryEnemies.createAnt();
-        ant.updateCoordinates2(new Point(2,2));
-        Point cordenates = new Point(3,3);
         TowerFactory factory = new WhiteTowerFactory();
+        Point cordenates = new Point(3,3);
         Tower WhiteTower = factory.createTower(cordenates);
+        Enemy antMock = mock(Ant.class);
+        when(antMock.getPoint()).thenReturn(new Point(9,9));
+        when(antMock.enemyDied()).thenReturn(true);
         ArrayList<Enemy> enemies = new ArrayList<>();
-        enemies.add(ant);
+        enemies.add(antMock);
 
         WhiteTower.constructionFinished();
         WhiteTower.attack(enemies);
 
-        Assertions.assertTrue(ant.enemyDied());
+        verify(antMock, atLeastOnce()).takeDamage(new Damage(1));
+        Assertions.assertTrue(antMock.enemyDied());
     }
 
     @Test
     void test04SilverTowerAttackWhenEnemyIsWithinRangeEnemyTakesDamage() {
-        EnemyFactory factoryEnemies = new EnemyFactory();
-        Enemy spider = factoryEnemies.createSpider();
-        spider.updateCoordinates2(new Point(2,2));
-        ArrayList<Enemy> enemies = new ArrayList<>();
-        enemies.add(spider);
-
         TowerFactory factory = new SilverTowerFactory();
         Point cordenates = new Point(3,3);
         Tower SilverTower = factory.createTower(cordenates);
+        Enemy spiderMock = mock(Spider.class);
+        when(spiderMock.getPoint()).thenReturn(new Point(4,4));
+        when(spiderMock.enemyDied()).thenReturn(true);
+        ArrayList<Enemy> enemies = new ArrayList<>();
+        enemies.add(spiderMock);
+
+
 
         SilverTower.constructionFinished();
         SilverTower.attack(enemies);
 
-        Assertions.assertTrue(spider.enemyDied());
+        verify(spiderMock, atLeastOnce()).takeDamage(new Damage(2));
+        Assertions.assertTrue(spiderMock.enemyDied());
     }
-*/
+
+    @Test
+    void test05SilverTowerCannotAttackAnEnemyOutOfRange() {
+        TowerFactory factory = new SilverTowerFactory();
+        Point cordenates = new Point(3,3);
+        Tower SilverTower = factory.createTower(cordenates);
+        Enemy spiderMock = mock(Spider.class);
+        when(spiderMock.getPoint()).thenReturn(new Point(4,4));
+        when(spiderMock.enemyDied()).thenReturn(false);
+        ArrayList<Enemy> enemies = new ArrayList<>();
+        enemies.add(spiderMock);
+
+
+
+        SilverTower.constructionFinished();
+        SilverTower.attack(enemies);
+
+        verify(spiderMock, never()).takeDamage(new Damage(1));
+        Assertions.assertFalse(spiderMock.enemyDied());
+    }
+
+    @Test
+    void test06SilverTowerCannotAttackAnEnemyOutOfRange() {
+        TowerFactory factory = new SilverTowerFactory();
+        Point cordenates = new Point(3,3);
+        Tower SilverTower = factory.createTower(cordenates);
+        Enemy spiderMock = mock(Spider.class);
+        when(spiderMock.getPoint()).thenReturn(new Point(9,9));
+        when(spiderMock.enemyDied()).thenReturn(false);
+        ArrayList<Enemy> enemies = new ArrayList<>();
+        enemies.add(spiderMock);
+
+
+
+        SilverTower.constructionFinished();
+        SilverTower.attack(enemies);
+
+        verify(spiderMock, never()).takeDamage(new Damage(2));
+        Assertions.assertFalse(spiderMock.enemyDied());
+    }
 }
