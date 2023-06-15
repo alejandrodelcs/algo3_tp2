b7d6af87e0006f59907cc500fb19e5162a14f665
alejandrodelcs <alejodc15@gmail.com>
2023-06-15 12:18:30 -0300

    refactor speed

---
+++ src/main/java/edu/fiuba/algo3/modelo/speed/Movement.java
@@ -0,0 +1,40 @@
+package edu.fiuba.algo3.modelo.speed;
+
+import edu.fiuba.algo3.modelo.enemy.Enemy;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/*
+public class Movement {
+    Move move;
+
+
+    private int moves;
+    private final int MOVES_NEEDED_FOR_SPEED_2;
+    private final int MOVES_NEEDED_FOR_SPEED_3;
+
+    private int positionInPath;
+
+    private ArrayList<Point> enemyPath;
+
+    public Movement(int positionInPath, ArrayList<Point> enemyPath){
+        this.positionInPath = positionInPath;
+        this.enemyPath = enemyPath;
+        this.moves = 0;
+        this.MOVES_NEEDED_FOR_SPEED_2 = 6;
+        this.MOVES_NEEDED_FOR_SPEED_3 = 11;
+    }
+    public void acelerate(){
+        moves++;
+        if (moves >= MOVES_NEEDED_FOR_SPEED_2 && moves < MOVES_NEEDED_FOR_SPEED_3) {
+            //speed = new Speed(2);
+        } else if (moves >= MOVES_NEEDED_FOR_SPEED_3){
+            //speed = new Speed(3);
+        }
+    }
+
+    public Point execute(Enemy enemy){
+        //return enemy.updateCoordinates(positionInPath, enemyPath);
+*/
+
