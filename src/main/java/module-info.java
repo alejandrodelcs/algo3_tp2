ca2a64b29e3f818dd03374766dde2e2a6bd8eb65
Ruyllex <ruymori630@gmail.com>
2023-06-22 09:44:24 -0300

    fixing requiriments

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module edu.fiuba.algo3 {
     requires javafx.controls;
     requires java.datatransfer;
     requires java.desktop;
-    requires javafx.fxml;
-    exports edu.fiuba.algo3;
-    exports edu.fiuba.algo3.modelo;
     requires json.simple;
+    requires javafx.fxml;
 
+    exports edu.fiuba.algo3 to javafx.fxml, javafx.graphics;
+    exports edu.fiuba.algo3.modelo;
+    opens edu.fiuba.algo3.controllers to javafx.fxml;
 }
