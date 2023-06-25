ca3cda14f55ea3fdb8214f3541c32b581aa8a397
Ruyllex <ruymori630@gmail.com>
2023-06-25 20:02:37 -0300

    print inside the box

--- src/main/java/edu/fiuba/algo3/controllers/Board.java
+++ src/main/java/edu/fiuba/algo3/controllers/Board.java
@@ -14,7 +14,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
