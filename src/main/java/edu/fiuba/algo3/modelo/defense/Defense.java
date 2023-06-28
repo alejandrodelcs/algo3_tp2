dccd8e0b76672ea4b660fdcd284bbab337bb1788
liu <odillette@gmail.com>
2023-06-27 23:12:30 -0300

    Refactor: make defense temporarylly state

--- src/main/java/edu/fiuba/algo3/modelo/defense/Defense.java
+++ src/main/java/edu/fiuba/algo3/modelo/defense/Defense.java
@@ -29,13 +29,9 @@ public abstract class Defense {
     public abstract ImageView getImage();
     public abstract String show();
     public boolean isAvailable() {
-        return (state.getClass() != DestructedState.class) && (state.getClass() == OperationalState.class);
+        return (state.getClass() != DestructedState.class) && (state.getClass() == OperationalState.class) || (state.getClass() != DestructedState.class) && (state.getClass() == TemporallyState.class);
     }
     public ArrayList<Point> getPlotsInRange() {
         return attack.getPlotsInRange(coordinates);
-    }
-
-    public boolean underConstruction() {
-        return state.getClass() != OperationalState.class;
     }
 }
